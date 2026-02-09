3222b6a0383d5be93ef06f132a2fa07aa08dd687
Andrei Anjos <andreiconrado13@gmail.com>
2026-02-09 15:16:55 +0000

    Criado função diga_ola()

--- ola.py
+++ ola.py
@@ -1,1 +1,12 @@
-print ("Olá Mundo!!!")
+from rich.console import Console
+
+console = Console()
+
+def diga_ola():
+    nome = input("\nDigite seu nome: ")
+    console.print (f"\n[blue]Olá {nome}, seja bem-vindo(a)![/blue]")
+    
+    idade = int(input("\nDigite sua idade: "))
+    console.print (f"\n[yellow]Perfeito {nome}. Você tem {idade} anos.[/yellow]\n")
+
+diga_ola()
