84d6cf20961ea60d05ea77c10bd9055f77a8694e
Andrei Anjos <andreiconrado13@gmail.com>
2026-03-23 14:31:09 +0000

    Criando lista3.py

--- util.py
+++ util.py
@@ -1,27 +1,31 @@
 def inputint(msg, min=None, max=None):
-    try:
-        valor = int(input(msg))
-        if min!=None and valor < min:
-            raise Exception(f"ERRO: Valor informado é menor do que o permitido de {min}!")
-        if max!=None and valor > max:
-            raise Exception(f"ERRO: Valor informado é maior do que o permitido de {max}!")
-        return valor
-    except ValueError:
-        print("Erro: Valor informado não é inteiro!")
-    except Exception as erro:
-        print(erro)
-    return -1
+    erro = True
+    while (erro == True):
+        try:
+            valor = int(input(msg))
+            if min!=None and valor < min:
+                raise Exception(f"ERRO: Valor informado é menor do que o permitido de {min}!")
+            if max!=None and valor > max:
+                raise Exception(f"ERRO: Valor informado é maior do que o permitido de {max}!")
+            erro = False
+            return valor
+        except ValueError:
+            print("Erro: Valor informado não é inteiro!")
+        except Exception as erro:
+            print(erro)
     
 def inputfloat(msg, min=None, max=None):
-    try:
-        valor = float(input(msg))
-        if min!=None and valor < min:
-            raise Exception(f"ERRO: Valor informado é menor do que o permitido de {min}!")
-        if max!=None and valor > max:
-            raise Exception(f"ERRO: Valor informado é maior do que o permitido de {max}!")
-        return valor
-    except ValueError:
-        print("Erro: Valor informado não é real!")
-    except Exception as erro:
-        print(erro)
-    return -1
+    erro = True
+    while (erro == True):
+        try:
+            valor = float(input(msg))
+            if min!=None and valor < min:
+                raise Exception(f"ERRO: Valor informado é menor do que o permitido de {min}!")
+            if max!=None and valor > max:
+                raise Exception(f"ERRO: Valor informado é maior do que o permitido de {max}!")
+            erro = False
+            return valor
+        except ValueError:
+            print("Erro: Valor informado não é real!")
+        except Exception as erro:
+            print(erro)
